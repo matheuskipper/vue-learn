e844fab616ac51558103b1a630e9d1f5a8830154
Matheus Kipper <matheuskipper9@gmail.com>
2025-02-03 23:18:48 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div class="bg-blue-500 p-4">
+    <h1 class="text-white">Bem-vindo ao Vue com Tailwind!</h1>
+  </div>
+</template>
+
+<style scoped></style>
