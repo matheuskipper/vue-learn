40119bcd2b8ad7b2506dc77000a6448d7ec037d0
Matheus Kipper <matheuskipper9@gmail.com>
2025-02-04 08:29:15 -0300

    adicionando elementos com Tailwind CSS

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,17 @@
   <div class="bg-blue-500 p-4">
     <h1 class="text-white">Bem-vindo ao Vue com Tailwind!</h1>
   </div>
+  <button
+    id="btn-1"
+    class="bg-red-600 text-white px-3 py-1 rounded-md hover:bg-red-700"
+  >
+    Botão
+  </button>
 </template>
 
-<style scoped></style>
+<style scoped>
+#btn-1 {
+  margin-top: 10px;
+  cursor: pointer;
+}
+</style>
