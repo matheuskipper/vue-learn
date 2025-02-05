b4fcae68840aa854a8b86c3981b7871de4042659
Matheus Kipper <matheuskipper9@gmail.com>
2025-02-05 16:09:34 -0300

    Consumindo uma API e exibindo na tela, além de adiocionar mais algumas estilizações com tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,60 @@
-<script setup></script>
-
 <template>
-  <div class="bg-blue-500 p-4">
-    <h1 class="text-white">Bem-vindo ao Vue com Tailwind!</h1>
+  <div class="container">
+    <h1 id="titulo" class="text-3xl font-bold p-4">
+      Personagens de Rick and Morty
+    </h1>
+    <div class="grid grid-cols-2 md:grid-cols-4 gap-4 p-5">
+      <div v-for="character in characters" :key="character.id" class="card">
+        <img
+          :src="character.image"
+          :alt="character.name"
+          class="rounded-lg shadow-lg"
+        />
+        <h2 class="text-xl font-semibold">{{ character.name }}</h2>
+        <p>Status: {{ character.status }}</p>
+      </div>
+    </div>
   </div>
-  <button
-    id="btn-1"
-    class="bg-red-600 text-white px-3 py-1 rounded-md hover:bg-red-700"
-  >
-    Botão
-  </button>
 </template>
 
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      characters: [],
+    };
+  },
+  async created() {
+    try {
+      const response = await axios.get(
+        "https://rickandmortyapi.com/api/character"
+      );
+      this.characters = response.data.results; // Pegando os personagens
+      console.log(this.characters);
+    } catch (error) {
+      console.error("Erro ao buscar personagens:", error);
+    }
+  },
+};
+</script>
+
 <style scoped>
-#btn-1 {
-  margin-top: 10px;
-  cursor: pointer;
+.container {
+  text-align: center;
+  background: #d5f5a2;
+  width: 100vw;
+  margin: 0;
+  padding: 0;
+}
+.card {
+  background: white;
+  padding: 15px;
+  border-radius: 10px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+#titulo {
+  color: aquamarine;
+  text-shadow: 2px 2px 4px #000000;
 }
 </style>
