126c56da75a72035771d91d1c7942f3b44e90ae8
Matheus Kipper <matheuskipper9@gmail.com>
2025-02-06 08:31:59 -0300

    implementando paginação no projeto

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,12 @@
       v-model="searchTerm"
       type="text"
       placeholder="Buscar personagem"
-      class="border-2 border-[#7FFFD4] p-2 m-2 rounded-md shadow-lg w-[20rem] mb-5"
+      class="border-2 border-[#7FFFD4] p-2 m-2 rounded-md shadow-lg w-[20rem] mb-5 mt-7"
     />
 
     <div class="grid grid-cols-2 md:grid-cols-4 gap-4 p-5">
       <div
-        v-for="character in filteredCharacters"
+        v-for="character in paginatedCharacters"
         :key="character.id"
         class="bg-[#FFFFFF] p-4 rounded-lg shadow-lg"
       >
@@ -26,6 +26,25 @@
         <p>Status: {{ character.status }}</p>
       </div>
     </div>
+    <div class="flex justify-center mt-5 gap-2 mb-5">
+      <button
+        @click="prevPage"
+        :disabled="currentPage === 1"
+        class="px-4 py-2 bg-gray-500 text-white rounded-md"
+      >
+        Anterior
+      </button>
+      <span class="text-lg font-semibold">
+        Página {{ currentPage }} de {{ totalPages }}
+      </span>
+      <button
+        @click="nextPage"
+        :disabled="currentPage === totalPages"
+        class="px-4 py-2 bg-gray-500 text-white rounded-md"
+      >
+        Próxima
+      </button>
+    </div>
   </div>
 </template>
 
@@ -36,6 +55,8 @@ export default {
     return {
       characters: [],
       searchTerm: "",
+      currentPage: 1,
+      itemsPerPage: 8,
     };
   },
   async created() {
@@ -44,7 +65,7 @@ export default {
         "https://rickandmortyapi.com/api/character"
       );
       this.characters = response.data.results; // Pegando os personagens
-      console.log(this.characters);
+      this.totalPages = Math.ceil(this.characters.length / this.itemsPerPage);
     } catch (error) {
       console.error("Erro ao buscar personagens:", error);
     }
@@ -54,6 +75,31 @@ export default {
       return this.characters.filter((character) =>
         character.name.toLowerCase().includes(this.searchTerm.toLowerCase())
       );
+    },
+    paginatedCharacters() {
+      const start = (this.currentPage - 1) * this.itemsPerPage;
+      const end = start + this.itemsPerPage;
+      return this.filteredCharacters.slice(start, end);
+    },
+    totalPages() {
+      return Math.ceil(this.filteredCharacters.length / this.itemsPerPage);
+    },
+  },
+  methods: {
+    prevPage() {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+      }
+    },
+    nextPage() {
+      if (this.currentPage < this.totalPages) {
+        this.currentPage++;
+      }
+    },
+  },
+  watch: {
+    searchTerm() {
+      this.currentPage = 1;
     },
   },
 };
